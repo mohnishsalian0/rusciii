deb3da94415706e516c19742606c7e06600b6115
Mohnish Salian <mohnishsalian02@gmail.com>
2024-05-13 23:18:06 +0530

    Bulk of ascii generator and leptos frontend

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![allow(dead_code, unused, non_snake_case, clippy::new_without_default)]
+
+pub mod app;
+pub mod asciiGenerator;
+pub mod components;
+pub mod imageHandler;
+pub mod sections;
+pub mod utils;
+
+use leptos::*;
+
+use crate::app::App;
+
 fn main() {
-    println!("Hello, world!");
+    console_error_panic_hook::set_once();
+    mount_to_body(|| view! { <App/> })
 }
